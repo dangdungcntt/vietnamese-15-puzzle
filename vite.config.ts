4be0509c7c4ba2cab072d56c92f1d7b9d41a10dc
Dung Nguyen Dang <dangdungcntt@gmail.com>
2022-10-02 19:12:20 +0700

    Add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Puzzle Game',
+                short_name: 'Puzzle Game',
+                description: 'Puzzle Game by Nddcoder',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: '/icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
     define: {
         '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
     }
