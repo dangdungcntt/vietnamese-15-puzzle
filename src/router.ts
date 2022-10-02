5b1bb793d1f0ba43f758437ab97ef1ea4b76b73f
Dung Nguyen Dang <dangdungcntt@gmail.com>
2022-10-02 10:17:10 +0700

    Restruct route path. setup redirect old path to new path

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, RouteLocation } from 'vue-router';
 import Home from './pages/Home.vue'
 import ImagePage from './pages/mode/ImagePage.vue'
 
 const routes = [
     { path: '/', component: Home },
-    { path: '/mode/image/:imageName', component: ImagePage, props: true },
-    { path: '/:mapSize', component: Home, props: true },
+    { path: '/i/:imageName', component: ImagePage, props: true },
+    { path: '/mode/image/:imageName', redirect: (to: RouteLocation) => ({ path: `/i/${to.params.imageName}` }) },
+    { path: '/c/:mapSize', component: Home, props: true },
+    { path: '/:mapSize', redirect: (to: RouteLocation) => ({ path: `/c/${to.params.mapSize}` }) },
 ]
 
 const router = createRouter({
